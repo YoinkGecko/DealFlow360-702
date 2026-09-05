2759df7ff953626fc8f85769d3c42274a0c03978
Kartikeya <kartikeya.anjul@gmail.com>
2026-09-05 10:13:40 +0530

    feat(middlewares) error,notfound, rateLimit, requestLogger

---
+++ backend/src/middleware/error.middleware.ts
@@ -0,0 +1,15 @@
+import type { Request, Response, NextFunction } from "express";
+
+export function errorMiddleware(
+  error: unknown,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  console.error(error);
+
+  res.status(500).json({
+    success: false,
+    message: "Internal server error",
+  });
+}
