f3618f13e0a8c3783a688c86ef518d47a0b827a2
Kartikeya <kartikeya.anjul@gmail.com>
2026-09-05 15:11:16 +0530

    refactor(approval) added authenticate and authorize in routes

--- backend/src/routes/approval.routes.ts
+++ backend/src/routes/approval.routes.ts
@@ -5,10 +5,24 @@ import {
   rejectApproval,
 } from "../controllers/approval.controller.js";
 
+import { authenticate } from "../middleware/auth.middleware.js";
+
+import { authorize } from "../middleware/authorize.middleware.js";
+
 const router = Router();
 
-router.post("/:id/approve", approveApproval);
+router.post(
+  "/:id/approve",
+  authenticate,
+  authorize("SALES_MANAGER", "FINANCE"),
+  approveApproval,
+);
 
-router.post("/:id/reject", rejectApproval);
+router.post(
+  "/:id/reject",
+  authenticate,
+  authorize("SALES_MANAGER", "FINANCE"),
+  rejectApproval,
+);
 
 export default router;
